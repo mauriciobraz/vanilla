2ded012678dad36faff02967a3c78c676074d8c2
Vesper <vesper@vanilla.localdomain>
2025-03-05 11:59:49 +0000

    fix: remove bugged fish config

--- includes/etc/fish-c/config.fish
+++ includes/etc/fish-c/config.fish
@@ -5,15 +5,3 @@ if type -q micro
     set -x EDITOR micro
     set -x VISUAL micro
 end
-
-if type -q vso
-    if not test -e /etc/fish/completions/vso.fish
-        vso completion fish > /etc/fish/completions/vso.fish
-    end
-end
-
-if type -q abroot
-    if not test -e /etc/fish/completions/abroot.fish
-        abroot completion fish > /etc/fish/completions/abroot.fish
-    end
-end
